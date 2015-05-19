645bffa65c433116fc35013413fbc299bd9c274f
Harold Serrano <harold.serrano@gmail.com>
2015-05-19 15:13:56 -0700

    Initial commit
    Initial Commit of pattern

---
+++ CompositeDesignPattern/CompositeDesignPattern/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CompositeDesignPattern
+//
+//  Created by Harold Serrano on 5/19/15.
+//  Copyright (c) 2015 cgdemy.com. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
